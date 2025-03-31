c9ae285fe3bf4a5dc4284ae9534ee8fd5c5b4eaa
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-03-31 16:32:06 -0400

    more cars

--- frontend/src/components/data/listings.js
+++ frontend/src/components/data/listings.js
@@ -2,11 +2,17 @@ import m5 from "../images/m5.jpg";
 import rs7 from "../images/rs7.jpg";
 import c63 from "../images/c63.jpg";
 import turbos from "../images/turbos.jpg"
+import m4 from "../images/m4.jpeg";
+import a3 from "../images/a3.jpg";
+import e63 from "../images/e63.jpg";
+import cayenne from "../images/cayenne.jpg";
+import golfR from "../images/golfr.jpg";
+import i4 from "../images/i4.jpg";
 
 const listings = [
   {
     id: 1,
-    title: "BMW M5",  
+    title: "BMW M5",
     make: "BMW",
     model: "M5",
     image: m5,
@@ -43,6 +49,66 @@ const listings = [
     year: "2021",
     mileage: "2,251",
     price: "189,999",
+  },
+  {
+    id: 5,
+    title: "BMW M4 Competition",
+    make: "BMW",
+    model: "M4",
+    image: m4,
+    year: "2022",
+    mileage: "6,910",
+    price: "104,499",
+  },
+  {
+    id: 6,
+    title: "Audi A3",
+    make: "Audi",
+    model: "A3",
+    image: a3,
+    year: "2021",
+    mileage: "17,850",
+    price: "38,999",
+  },
+  {
+    id: 7,
+    title: "Mercedes-AMG E63 S",
+    make: "Mercedes-Benz",
+    model: "E63 S",
+    image: e63,
+    year: "2022",
+    mileage: "10,402",
+    price: "129,999",
+  },
+  {
+    id: 8,
+    title: "Porsche Cayenne GTS",
+    make: "Porsche",
+    model: "Cayenne GTS",
+    image: cayenne,
+    year: "2023",
+    mileage: "4,700",
+    price: "123,450",
+  },
+  {
+    id: 9,
+    title: "Volkswagen Golf R",
+    make: "Volkswagen",
+    model: "Golf R",
+    image: golfR,
+    year: "2023",
+    mileage: "2,090",
+    price: "49,999",
+  },
+  {
+    id: 10,
+    title: "BMW i4 M50",
+    make: "BMW",
+    model: "i4",
+    image: i4,
+    year: "2023",
+    mileage: "3,100",
+    price: "78,500",
   },
 ];
 
