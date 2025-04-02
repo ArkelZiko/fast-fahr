de6546742ceffc8163311e382950d81866a6a540
Yusuf Alam <0yusufalam@gmail.com>
2025-04-02 17:12:17 -0400

    bug w/ the navbar on hompage and selling for windows

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/Navbar";
 import Card from "../components/Card";
 import Filters from "../components/Filters";
 import filterListings from "../components/filterListingsComponent/filterListings";
