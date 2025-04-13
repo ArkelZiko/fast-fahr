35a065f1e600422610fb5f520852ac5300d09d71
Yusuf Alam <0yusufalam@gmail.com>
2025-04-13 00:53:22 -0400

    environment variable file with dotenv (installed with composer) now makes connect.php portable from diff users and server

--- backend/config/Connect.php
+++ backend/config/Connect.php
@@ -1,16 +1,26 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+
 // CONNECT TO DATABASE, use  include 'connect.php';   in other backend filess that need database connection
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+var_dump($_ENV); //quick lil vardump never hurt
+exit;
+
 
 try {
     $dbh = new PDO(
-        "mysql:host=localhost;dbname=zikoa_db",
-        "root",
-        "" // 8/hYM9U4jOCMWpc_
+        "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}",
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS']
     );
 
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("ERROR: Couldn't connect. {$e->getMessage()}");
 }
-?>
